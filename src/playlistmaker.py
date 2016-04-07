d716a53ece7af49bc7b2b5f930153ab0c4b1b9af
Isabelle Ferreira <isabellecfer@gmail.com>
2016-04-07 01:54:14 -0300

    Improving the playlistmaker by artists

--- src/playlistmaker.py
+++ src/playlistmaker.py
@@ -3,6 +3,8 @@ import json
 import os.path
 import random
 import urllib
+import urlparse
+import re
 
 from setmap import setmap
 
@@ -36,10 +38,10 @@ def makeplaylist(list):
 
         try:
             for artist in res["topartists"]["artist"]:
-                artist_map.add(artist["name"],1)
+                artist_map.add(artist["name"],usuario)
                 playcount_map.add(artist["name"],int(artist["playcount"]))
 
-        except: print "Usuario invalido"
+        except: print "Usuario invalido"+usuario
     con.close()
 
     if len(artist_map) == 0:
@@ -58,35 +60,61 @@ def makeplaylist(list):
         maxn = sum(playcount_map.get(plays))
         if maxn > maxt:
             maxt = maxn
-    print maxt
 
     for artist in artist_map:
-        pontuacao = 0.80*artist_map.count(artist)/(maxa) + 0.20*sum(playcount_map.get(artist))/maxt
+        pontuacao = 0.90*artist_map.count(artist)/(maxa) + 0.10*sum(playcount_map.get(artist))/maxt
         scorelist.append((artist, pontuacao))
-        print pontuacao
-        print artist
     scorelist.sort(key=lambda tup: -tup[1])
-    
-    for s in scorelist:
-        if s[1] >= 0.82:
-            toplist.append(s[0])
 
-    if len(toplist) > 40:
-        toplist = random.sample(toplist, 40)
-    random.shuffle(toplist)
-    print(toplist)
+    # Choose top 40 artists
+    topart = []
+    scorelist = scorelist[:40]
+    for artist in scorelist:
+        topart.append(artist[0])
 
-    print "Recuperando IDs"
+    con = httplib.HTTPConnection("ws.audioscrobbler.com")
+    songs = []
+    print "Requisitando melhores musicas de:"
+    for artist in topart:
+        print "..."+artist
 
-    import urllib
-    import urlparse
-    import re
-    
+        if savelocally and os.path.isfile("local/"+artist+".json"):
+            file = open("local/"+artist+".json")
+            res = json.loads(file.read())
+            file.close()
+        else:
+            artistF = urllib.urlencode({'artist':artist.encode("utf-8")})
+            con.request("GET", "/2.0?method=artist.getTopTracks&format=json&api_key="+key+"&"+artistF+"&limit=20")
+            res = json.loads(con.getresponse().read())
+
+            if savelocally:
+                if not os.path.exists("local"): os.makedirs("local")
+                file = open("local/"+artist+".json", "w")
+                file.write(json.dumps(res))
+                file.close()
+
+        try:
+            for song in res["toptracks"]["track"]:
+                songs.append((artist,song["name"]))
+
+        except: print "Artista invalido: "+artist
+    con.close()
+
+    # Randomly choose 30
+    toplist = random.sample(songs,100)
+    toplist = random.sample(songs,30)
+
+    #for song in toplist:
+    #    print("Artista: "+song[0]+". Musica: "+song[1])
+    #print toplist
+
+    # Search top 30 on YouTube
+    print "Procurando musicas no youtube!"
     ids = []
     for s in toplist:
         title = unicode(s[1].replace(" ","+")).encode("utf-8")
         artist = unicode(s[0].replace(" ","+")).encode("utf-8")
-        print("Searching for: "+title+"+"+artist)
+        #print("Searching for: "+title+"+"+artist)
         query_string = "search_query="+title+"+"+artist
 
         html_content = urllib.urlopen("http://www.youtube.com/results?" + query_string+"&max-results=1&category=music")
@@ -94,7 +122,7 @@ def makeplaylist(list):
 
         try:
             ids.append(search_results[0])
-            print("Found: "+search_results[0])
+            #print("Found: "+search_results[0])
 
         except: pass   
     print("Terminou adicionar musicas a lista")
